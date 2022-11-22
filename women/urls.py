4b0927fa5f6d49b031212cdac2de6a5fb4ffe496
INlOEtdF <krasheg@gmail.com>
2022-11-22 16:02:57 +0200

    added new functionality

--- women/urls.py
+++ women/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
--- women/views.py
+++ women/views.py
@@ -2,25 +2,44 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from .models import *
 
-
 # Create your views here.
+menu = [{'title': 'About', 'url_name': 'about'},
+        {'title': 'Add post', 'url_name': 'add_page'},
+        {'title': 'Contacts', 'url_name': 'contact'},
+        {'title': 'Login', 'url_name': 'login'},
+        ]
+
+
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'title': 'Main page'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': "Main page"
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'title': "About"})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f'Categories for {cat_id=}')
+def addpage(request):
+    return HttpResponse('Adding a new post')
 
 
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('home', permanent=True)
+def contact(request):
+    return HttpResponse('Contact us')
 
+
+def login(request):
+    return HttpResponse('Authorization')
+
+
+def show_post(request, post_id):
+    obj = Women.objects.filter(id=post_id).first()
+    text = obj.content
+    return HttpResponse(f"{text}")
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('Страница не найдена')
