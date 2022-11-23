8b9f04247e493adaa5a77a6257c04ac6ee262ff5
INlOEtdF <krasheg@gmail.com>
2022-11-23 12:44:53 +0200

    set admin.py

--- women/models.py
+++ women/models.py
@@ -8,7 +8,7 @@ class Women(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True, verbose_name='published')
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
@@ -17,9 +17,17 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        ordering = ['time_create', 'title']
+        verbose_name_plural = "Women"
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Category')
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
